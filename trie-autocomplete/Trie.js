84f34b20ecf94cbd771265b4810b807b509525a0
stephenau93 <stephenau93@gmail.com>
2019-12-20 04:28:04 -0800

    Add radix-sort

---
+++ trie-autocomplete/Trie.js
@@ -0,0 +1,21 @@
+// Autocomplete class implemented with a Trie data structure
+// dict: {"abc", "acd", "bcd", "def", "a", "aba"}
+
+/*
+          O
+       a/   \b
+       O 
+     b/ \c
+*/
+
+
+class Autocomplete {
+
+  constructor(prefix) {
+    this.prefix = prefix;
+    this.children = {};
+  }
+
+  
+
+}

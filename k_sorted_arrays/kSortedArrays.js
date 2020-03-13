20483eb87d4c2249b85175ed28437f0f6002877a
stephenau93 <stephenau93@gmail.com>
2020-03-13 01:42:26 -0700

    Add priority queue solution

--- k_sorted_arrays/kSortedArrays.js
+++ k_sorted_arrays/kSortedArrays.js
@@ -60,7 +60,7 @@ class MinHeap {
     return this.heap[0];
   }
 
-  min() {
+  getMin() {
     let min = this.heap[0];
     this.heap[0] = this.heap.pop();
     this.bubbleDown(0);
@@ -68,36 +68,46 @@ class MinHeap {
 
   bubbleUp(idx) {
     while (idx > 0) {
-      let parentIdx = Math.floor((idx + 1) / 2) - 1;
+      let parentIdx = this.getParent(idx);
       if (this.heap[parentIdx] > this.heap[idx]) {
         [this.heap[parentIdx], this.heap[idx]] = [this.heap[idx], this.heap[parentIdx]];
       };
       idx = parentIdx;
+      this.bubbleUp(idx);
     }
   }
 
+  getParent(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  getLeftChild(idx) {
+    return idx * 2 + 1;
+  }
+
+  getRightChild(idx) {
+    return idx * 2 + 2;
+  }
+
+  hasLeftChild(idx) {
+    return this.getLeftChild(idx) < this.heap.length;
+  }
+
+  hasRightChild(idx) {
+    return this.getRightChild(idx) < this.heap.length;
+  }
+
   bubbleDown(idx) {
-    while (true) {
-      let right = (idx + 1) * 2;
-      let left = right - 1;
+    while (this.hasLeftChild(idx)) {
+      let left = this.getLeftChild(idx);
+      let right = this.getRightChild(idx);
       let swapIdx;
 
-      if (this.heap[idx] > this.heap[right]) {
+      if (this.hasRightChild(idx) && (this.heap[right] < this.heap[left])) {
         swapIdx = right;
       }
 
-      if (this.heap[right] === null) {
-        if (this.heap[left] !== null) {
-          swapIdx = left;
-        }
-      }
-
-      if (this.heap[idx] > this.heap[left] && (this.heap[right] === null || (this.heap[right] !== null && this.heap[left] < this.heap[right]))) {
-        swapIdx = left;
-      }
-
-      if (swapIdx === null) break;
-
+      if (this.heap[idx] < this.heap[swapIdx]) break;
       [this.heap[swapIdx], this.heap[idx]] = [this.heap[idx], this.heap[swapIdx]];
 
       idx = swapIdx;
@@ -112,30 +122,171 @@ class MinHeap {
 }
 
 
+// const mergeKSortedArrays = (arrays) => {
+//   let output = [];
+//   let minHeap = new MinHeap();
+//   for (let i = 0; i < arrays.length; i++) {
+//     let n = 0;
+//     while (n < arrays[i].length) {
+//       minHeap.insert(arrays[i][n]);
+//       console.log(`minHeap${i}${n}: ${minHeap.heap}`)
+//       n++;
+//     };
+//     n = 0;
+//   };
+
+//   console.log("minHEAP: ", minHeap);
+//   while (!minHeap.isEmpty()) {
+//     let minVal = minHeap.min();
+//     output.push(minVal);
+//   }
+
+//   return output;
+// }
+
+// let arrs = [
+//   [2, 6, 12, 34],
+//   [1, 9, 20, 1000],
+//   [23, 34, 90, 2000]
+// ];
+
+// console.log(mergeKSortedArrays(arrs));
+
+
+
+/***
+ * 
+ * Priority Queue implementation
+ * https://stackoverflow.com/questions/42919469/efficient-way-to-implement-priority-queue-in-javascript/42919752
+ * 
+ */
+
+class QueueNode {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  };
+}
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = [];
+    this._comparator = comparator
+  }
+
+  size() {
+    return this.heap.length;
+  };
+
+  isEmpty() {
+    return this.size() === 0;
+  };
+
+  peek() {
+    return this.heap[0];
+  };
+
+  getParent(idx) {
+    return Math.floor(idx / 2)
+  }
+
+  getLeftChild(idx) {
+    return idx * 2 + 1;
+  }
+
+  getRightChild(idx) {
+    return idx * 2 + 2;
+  }
+  hasLeftChild(idx) {
+    return this.getLeftChild(idx) < this.heap.length;
+  }
+
+  hasRightChild(idx) {
+    return this.getRightChild(idx) < this.heap.length;
+  }
+
+  push(val) {
+    this.heap.push(val);
+    this.siftUp();
+    return this.size();
+  }
+
+  pop() {
+    let top = 0;
+    let poppedVal = this.peek();
+    let bottom = this.size() - 1;
+    if (bottom > top) this.swap(top, bottom);
+    this.heap.pop();
+    this.siftDown();
+    return poppedVal;
+  }
+
+
+  replace(val) {
+    let replacedVal = this.peek();
+    this.heap[0] = val;
+    this.siftDown();
+  }
+
+  greater(i, j) {
+    return this._comparator(this.heap[i], this.heap[j]);
+  };
+
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  };
+
+  siftUp() {
+    let nodeIdx = this.size() - 1;
+    let parentIdx = this.getParent(nodeIdx)
+    while (nodeIdx > 0 && this.greater(nodeIdx, parentIdx)) {
+      this.swap(nodeIdx, parentIdx);
+      nodeIdx = parentIdx;
+    };
+  };
+
+
+  siftDown() {
+    let nodeIdx = 0;
+    let leftIdx = this.getLeftChild(nodeIdx);
+    let rightIdx = this.getRightChild(nodeIdx);
+    while (
+      (leftIdx < this.heap.length && this.greater(leftIdx, nodeIdx)) || 
+      (rightIdx < this.heap.length && this.greater(rightIdx, nodeIdx))
+    ) {
+      let maxChildIdx = ( rightIdx < this.heap.length && this.greater(rightIdx, leftIdx) ) ? rightIdx : leftIdx;
+      this.swap(nodeIdx, maxChildIdx);
+      nodeIdx = maxChildIdx;
+    }
+  }
+}
+
+let arrs = [
+  [2, 6, 12, 34],
+  [1, 9, 20, 1000],
+  [23, 34, 90, 2000]
+];
+
 const mergeKSortedArrays = (arrays) => {
   let output = [];
-  let minHeap = new MinHeap();
+  let queue = new PriorityQueue();
   for (let i = 0; i < arrays.length; i++) {
     let n = 0;
-    while (n < arrays.length) {
-      minHeap.insert(arrays[i][n]);
+    while (n < arrays[i].length) {
+      queue.push(arrays[i][n]);
+      console.log(queue);
       n++;
     };
     n = 0;
   };
 
-  console.log("minHEAP: ", minHeap);
-  while (!minHeap.isEmpty()) {
-    let minVal = minHeap.min();
+  while (!queue.isEmpty()) {
+    let minVal = queue.pop();
     output.push(minVal);
   }
 
   return output;
 }
 
-let arrs = arr = [
-  [2, 6, 12, 34],
-  [1, 9, 20, 1000],
-  [23, 34, 90, 2000]
-];
-console.log(mergeKSortedArrays(arrs));
+let h = (mergeKSortedArrays(arrs));
+console.log(h);
--- min-heap/minHeap.js
+++ min-heap/minHeap.js
@@ -1,77 +1,77 @@
 class MinHeap {
-  constructor(arr) {
-    this.heap = [null]
+  /**
+   * Min-Heap Class
+   */
+  constructor() {
+    this.heap = [];
   }
 
   insert(val) {
     this.heap.push(val);
-    this.bubbleUp(this.heap.length - 1)
-  }
-
-  bubbleUp(idx) {
-    if (idx === 1) return;
-    let parentIdx = this.getParent(idx);
-    // let curr = this.heap[idx];
-    if (this.heap[parentIdx] > this.heap[idx]) {
-      // if parent node is greater than current node, swap them and check again
-      [this.heap[parentIdx], this.heap[idx]] = [this.heap[idx], this.heap[parentIdx]];
-      this.bubbleUp(parentIdx);
-    };
-  };
-
-  bubbleDown(i) {
-    let heapCopy = this.heap;
-    let leftChildIdx = this.getLeftChild(i);
-    let rightChildIdx = this.getRightChild(i);
-    let leftVal = this.heapCopy[leftChildIdx];
-    let rightVal = this.heapCopy[rightChildIdx];
-
-    if (leftVal === undefined) leftVal = -Infinity;
-    if (rightVal === undefined) rightVal = -Infinity;
-
-    if (heapCopy[i] < leftVal && heapCopy[i] < rightVal) return;
-
-    let swapIdx;
-    // Set the swap index to be the value
-    if (rightVal < leftVal) {
-      swapIdx = rightChildIdx;
-    } else {
-      swapIdx = leftChildIdx;
-    }
-
-    [heapCopy[i], heapCopy[swapIdx]] = [heapCopy[swapIdx], heapCopy[i]];
-    this.siftDown(swapIdx);
-  }
-
-
-  getLeftChild(idx) {
-    return idx * 2;
-  }
-
-  getRightChild(idx) {
-    return idx * 2 + 1;
-  }
-
-  getParent(idx) {
-    return Math.floor(idx / 2);
+    this.bubbleUp(this.heap.length - 1);
   }
 
   peek() {
-    return this.heap[1];
+    return this.heap[0];
+  }
+
+  getMin() {
+    let min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown(0);
+  }
+
+  bubbleUp(idx) {
+    while (idx > 0) {
+      let parentIdx = this.getParent(idx);
+      if (this.heap[parentIdx] > this.heap[idx]) {
+        [this.heap[parentIdx], this.heap[idx]] = [this.heap[idx], this.heap[parentIdx]];
+      };
+      idx = parentIdx;
+      this.bubbleUp(idx);
+    }
+  }
+
+  getParent(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  getLeftChild(idx) {
+    return idx * 2 + 1;
+  }
+
+  getRightChild(idx) {
+    return idx * 2 + 2;
+  }
+
+  hasLeftChild(idx) {
+    return this.getLeftChild(idx) < this.heap.length;
+  }
+
+  hasRightChild(idx) {
+    return this.getRightChild(idx) < this.heap.length;
+  }
+
+  bubbleDown(idx) {
+    while (this.hasLeftChild(idx)) {
+      let left = this.getLeftChild(idx);
+      let right = this.getRightChild(idx);
+      let swapIdx;
+
+      if (this.hasRightChild(idx) && (this.heap[right] < this.heap[left])) {
+        swapIdx = right;
+      }
+
+      if (this.heap[idx] < this.heap[swapIdx]) break;
+      [this.heap[swapIdx], this.heap[idx]] = [this.heap[idx], this.heap[swapIdx]];
+
+      idx = swapIdx;
+    }
   }
 
 
-
-  pop() {
-    if (this.heap.length === 2) return this.heap.pop();
-    if (this.heap.length === 1) return this.heap[0]; // null
-    let min = this.heap[1];
-    // set the min val at top of heap to be the last val in our heap  
-    this.heap[1] = this.heap.pop();
-    // then bubble it down to balance our heap
-    this.bubbleDown(1);
-    return min;
+  isEmpty() {
+    return this.heap.length > 0;
   }
 
 }
-
